d09d38e1e36dfb96a45ddc160ab99b52439ca8a9
Larin Ilya <1hersil88@gmail.com>
2025-08-13 18:33:18 +0600

    Change a recording simbol to nf-cod-record

--- .config/hypr/scripts/recording_status.sh
+++ .config/hypr/scripts/recording_status.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 if pgrep -x "wf-recorder" > /dev/null; then
-  echo '{"text": "🔴 REC", "tooltip": "Recording active", "class": "recording-active"}'
+  echo '{"text": "  REC", "tooltip": "Recording active", "class": "recording-active"}'
 else
   echo '{"text": "", "tooltip": "", "class": "recording-inactive"}'
 fi
